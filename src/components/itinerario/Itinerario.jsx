c32ae03530c6003308a27d1d1ef04598a9712fe1
sofigandulfo <sofigandulfo@gmail.com>
2025-12-05 16:46:00 -0300

    cambio de ruta en img prueba

--- src/components/itinerario/Itinerario.jsx
+++ src/components/itinerario/Itinerario.jsx
@@ -4,7 +4,7 @@ function Itinerario(){
     return(
         <>
             <h2 className='itinerario-titulo'>Así se vive nuestro "sí, acepto"</h2>
-            <img src="/src/assets/asi se vive nuestro si acepto.png" alt="" className="itinerario-img"/>
+            <img src="../../assets/asi se vive nuestro si acepto.png" alt="" className="itinerario-img"/>
             <img src='/src/assets/4to separador de textos desp de cronograma del evento.png' className='separador-grande'></img>
         </>
     )
