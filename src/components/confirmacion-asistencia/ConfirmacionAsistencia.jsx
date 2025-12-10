5c1b92cc8a539b2ec62fb458f7e159ddcb62f133
sofigandulfo <sofigandulfo@gmail.com>
2025-12-10 19:16:29 -0300

    confirmacion asistencia whatsapps

--- src/components/confirmacion-asistencia/ConfirmacionAsistencia.jsx
+++ src/components/confirmacion-asistencia/ConfirmacionAsistencia.jsx
@@ -8,13 +8,17 @@ function ConfirmacionAsistencia(){
             <div className="confirmacion-container">
                 
                 <div className="contacto-item">
-                    <p>CONTACTAR A LA NOVIA</p>
-                    <img src='/logo WhatsApp.png' className="whatsapp-logo" />
+                    <a href="https://wa.me/5491167262223?text=Hola!%20Confirmo%20mi%20asistencia" target="_blank" rel="noopener noreferrer" className="contacto-item">
+                        <p>CONTACTAR A LA NOVIA</p>
+                        <img src='/logo WhatsApp.png' className="whatsapp-logo" />
+                    </a>
                 </div>
 
                 <div className="contacto-item">
-                    <p>CONTACTAR AL NOVIO</p>
-                    <img src='/logo WhatsApp.png' className="whatsapp-logo" />
+                    <a href="https://wa.me/5491168577745?text=Hola!%20Confirmo%20mi%20asistencia" target="_blank" rel="noopener noreferrer" className="contacto-item">
+                        <p>CONTACTAR AL NOVIO</p>
+                        <img src='/logo WhatsApp.png' className="whatsapp-logo" />
+                    </a>
                 </div>
             </div>
                 <img src='/separador de textos chico.png' alt='Separador' className='separador-chico'></img>
