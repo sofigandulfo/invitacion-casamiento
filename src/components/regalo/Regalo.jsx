c990876e3478dc578df22066939ad45792254c0b
sofigandulfo <sofigandulfo@gmail.com>
2025-12-23 01:12:48 -0300

    añadido de metodo de pago

--- src/components/regalo/Regalo.jsx
+++ src/components/regalo/Regalo.jsx
@@ -1,6 +1,33 @@
+import { useState } from 'react'
 import './Regalo.css'
 
-function Regalo(){
+function Regalo() {
+  const cuentas = [
+    {
+      id: "pesos",
+      titulo: "Pesos",
+      cbu: "007001463000403505401",
+      alias: "GOLF.ALARMA.BRASIL"
+    },
+    {
+      id: "dolares",
+      titulo: "Dólares",
+      cbu: "00700146-31004033340957",
+      alias: "LLUVIA.BALADA.JAGUAR"
+    }
+  ]
+
+  const [copiado, setCopiado] = useState(null)
+
+  function copiar(texto, key) {
+    navigator.clipboard.writeText(texto)
+    setCopiado(key)
+
+    setTimeout(() => {
+      setCopiado(null)
+    }, 2000)
+  }
+
     return(
         <>
             <h2 className="regalo-titulo">Sugerencia de Regalo</h2>
@@ -21,6 +48,45 @@ function Regalo(){
                 ¡Y nos lleva al extranjero!
             </p>
 
+             <div className="cbu-card">
+            <h2 className="regalo-aclaracion">
+                Transferencia bancaria
+            </h2>
+        {cuentas.map((cuenta) => (
+    <div key={cuenta.id} className="cuenta-bloque">
+      <h3 className="cuenta-titulo">{cuenta.titulo}</h3>
+
+      {/* CBU */}
+      <div className="dato-grupo">
+        <div className="dato-copiable">
+        <span className="dato-etiqueta">CBU</span>
+          <span className="dato-valor">{cuenta.cbu}</span>
+          <img
+            src={copiado === `${cuenta.id}-cbu` ? "/check.svg" : "/copy.svg"}
+            className={`icono-copiar ${copiado === `${cuenta.id}-cbu` ? "copiado" : ""}`}
+            onClick={() => copiar(cuenta.cbu, `${cuenta.id}-cbu`)}
+            alt="Copiar CBU"
+          />
+        </div>
+      </div>
+
+      {/* Alias */}
+      <div className="dato-grupo">
+        <div className="dato-copiable">
+        <span className="dato-etiqueta">Alias</span>
+          <span className="dato-valor">{cuenta.alias}</span>
+          <img
+            src={copiado === `${cuenta.id}-alias` ? "/check.svg" : "/copy.svg"}
+            className={`icono-copiar ${copiado === `${cuenta.id}-alias` ? "copiado" : ""}`}
+            onClick={() => copiar(cuenta.alias, `${cuenta.id}-alias`)}
+            alt="Copiar Alias"
+          />
+        </div>
+      </div>
+    </div>
+  ))}
+      </div>
+
             <div className="regalo-separador-container">
                 <img 
                     src="/separador de textos chico.png" 
@@ -32,4 +98,4 @@ function Regalo(){
     )
 }
 
-export default Regalo;
+export default Regalo
